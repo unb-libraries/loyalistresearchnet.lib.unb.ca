c6613cf2f8339d541167d32903f07c617ebe2ae5
Jacob Sanford <jsanford@unb.ca>
2017-11-20 13:46:17 -0400

    Set Minimum Stability Across the Board

---
+++ tests/backstop/prod/backstop_data/html_report/config.js
@@ -0,0 +1,68 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
+        "label": "unbherbarium.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
+        "label": "unbherbarium.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
+        "label": "unbherbarium.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
