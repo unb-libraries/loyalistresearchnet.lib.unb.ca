be01d4f8b25a4ce72ba7224d1bfc99c86660c99f
Jacob Sanford <jsanford@unb.ca>
2017-11-20 13:47:22 -0400

    Update visual regression reference images for dev

---
+++ tests/backstop/dev/backstop_data/html_report/config.js
@@ -0,0 +1,44 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171120-174658/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
+        "label": "dev-loyalistrsearchnet.org Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171120-174658/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
+        "label": "dev-loyalistrsearchnet.org Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20171120-174658/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
+        "selector": "document",
+        "fileName": "loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
+        "label": "dev-loyalistrsearchnet.org Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_dev-loyalistrsearchnetorg_Homepage_0_document_2_monitor.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
