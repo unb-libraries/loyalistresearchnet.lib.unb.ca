5c538ba152b77568582bee25e4f3776d0747b6f5
Jacob Sanford <jsanford@unb.ca>
2017-11-20 13:47:47 -0400

    Update visual regression reference images for prod

--- tests/backstop/prod/backstop_data/html_report/config.js
+++ tests/backstop/prod/backstop_data/html_report/config.js
@@ -3,66 +3,42 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171120-174740/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_0_phone.png",
-        "label": "unbherbarium.lib.unb.ca Homepage",
+        "fileName": "loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_0_phone.png",
+        "label": "loyalistrsearchnet.org Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "getDiffImage": null
-        }
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171120-174740/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
         "selector": "document",
-        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_1_tablet.png",
-        "label": "unbherbarium.lib.unb.ca Homepage",
+        "fileName": "loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_1_tablet.png",
+        "label": "loyalistrsearchnet.org Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "getDiffImage": null
-        }
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
-        "test": "../bitmaps_test/20171117-190101/loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
+        "reference": "../bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20171120-174740/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
         "selector": "document",
-        "fileName": "loyalistresearchnet_org_unbherbariumlibunbca_Homepage_0_document_2_monitor.png",
-        "label": "unbherbarium.lib.unb.ca Homepage",
+        "fileName": "loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_2_monitor.png",
+        "label": "loyalistrsearchnet.org Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "getDiffImage": null
-        }
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/loyalistresearchnet_org_loyalistrsearchnetorg_Homepage_0_document_2_monitor.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ]
 });
